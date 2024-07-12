65b9f8efb2657b16b7a8bdf00a115d735aed3c4d
Lavisha <lavishajagwani@gmail.com>
2024-07-12 17:20:54 +0530

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,5 +1,35 @@
 const { JSDOM } = require('jsdom') //lets us access DOM api's
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`)
+
+    try {
+        
+        //a fetch request to the url, by default fetch does a GET request
+        const resp = await fetch(currentURL) 
+
+        //condition to check the status code of the response
+        if(resp.status > 399) {
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`)
+            return
+        }
+
+        //condition to check wether the response is html or not
+        const contentType = resp.headers.get("content-type")
+        if(!contentType.includes("text/html")) {
+            console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
+            return
+        }
+
+        // .text because we are expecting resp to in html format
+        console.log(await resp.text()) 
+
+    } catch(err) {
+        console.log(`error in fetch: ${err.message} on page ${currentURL}`)
+    }
+    
+}
+
 //htmlBody -> html of the page, baseURL -> url of the page we are going to crawl
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = []
@@ -38,5 +68,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const { crawlPage } = require('./crawl.js')
+
+// process.argv lets us get the input from the command line
+function main() {
+    if(process.argv.length < 3) {
+        console.log("no website provided")
+        process.exit(1)
+    }
+
+    if(process.argv.length > 3) {
+        console.log("too many command line arguments")
+        process.exit(1)
+    }
+
+    // argv is an array of length 3 (name of interpreter, name of the file that is running, url provided)
+    const baseURL = process.argv[2]
+
+    console.log(`starting crawl of ${baseURL}`)
+    crawlPage(baseURL)
+}
+
+main()
